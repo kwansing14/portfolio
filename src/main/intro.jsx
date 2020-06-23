d4bc8fc7b3e02d105d1ecc3cb0340d678aba9238
kwansing14 <kwansing@hotmail.sg>
2020-06-23 18:33:08 +0800

    first commit

---
+++ src/main/intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Intro() {
+  return (
+    <div>
+      <h2>Hi my name is Kwan Sing,</h2>
+      <h2>a full stack developer seeking job opportunities.</h2>
+      <button>View my work</button>
+    </div>
+  );
+}
+
+export default Intro;
---
+++ src/main/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Intro from './intro';
+
+function Main() {
+  return (
+    <div>
+      <h1>hello world !</h1>
+      <Intro/>
+    </div>
+  );
+}
+
+export default Main;
