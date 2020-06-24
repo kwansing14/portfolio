e2d32d45179a991fe804dcc47325635d40d177d7
kwansing14 <kwansing@hotmail.sg>
2020-06-24 11:20:40 +0800

    Updates

---
+++ precache-manifest.87ce1045c8954091260b8be011e93138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fff48388d4bf76f624ea2e7b0f26af0",
+    "url": "/kwansing14.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "647b58994d325e44ad40",
+    "url": "/kwansing14.github.io/portfolio/static/css/main.0339cf06.chunk.css"
+  },
+  {
+    "revision": "b872efb25f8cf196ae76",
+    "url": "/kwansing14.github.io/portfolio/static/js/2.9778e4b7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kwansing14.github.io/portfolio/static/js/2.9778e4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "647b58994d325e44ad40",
+    "url": "/kwansing14.github.io/portfolio/static/js/main.af7419b7.chunk.js"
+  },
+  {
+    "revision": "cc58b3cb73284874be98",
+    "url": "/kwansing14.github.io/portfolio/static/js/runtime-main.ddc7f661.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.345e2d23327ed91ae12ac550d5956f85.js"
+  "/kwansing14.github.io/portfolio/precache-manifest.87ce1045c8954091260b8be011e93138.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kwansing14.github.io/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
