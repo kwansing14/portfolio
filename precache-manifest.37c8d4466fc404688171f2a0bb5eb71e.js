0bc565fa6216fa5d06e701896de264840a6f79c5
kwansing14 <kwansing@hotmail.sg>
2020-06-25 18:39:55 +0800

    Updates

--- precache-manifest.37c8d4466fc404688171f2a0bb5eb71e.js
+++ precache-manifest.37c8d4466fc404688171f2a0bb5eb71e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7de663088b0575dbb6bf4f0befa51a2",
+    "revision": "a2ffdb480599c91f10e7c95b9973d7fd",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "27a2340fe6a56d408829",
+    "revision": "d01a4feb4273505e80ff",
     "url": "/portfolio/static/css/main.0339cf06.chunk.css"
   },
   {
-    "revision": "9a4568a32d5a6bafafce",
-    "url": "/portfolio/static/js/2.f5f2909a.chunk.js"
+    "revision": "f6bcea3f470304d8f884",
+    "url": "/portfolio/static/js/2.6ccd6c4c.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/portfolio/static/js/2.f5f2909a.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.6ccd6c4c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "27a2340fe6a56d408829",
-    "url": "/portfolio/static/js/main.07c11dd1.chunk.js"
+    "revision": "d01a4feb4273505e80ff",
+    "url": "/portfolio/static/js/main.6c1bff35.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2a284b12b313f79ea69fc40e59367cb9.js"
+  "/portfolio/precache-manifest.37c8d4466fc404688171f2a0bb5eb71e.js"
 );
 
 self.addEventListener('message', (event) => {
